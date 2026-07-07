2b3c563aacb8d8efdccd9aa54971135eef899455
VoVik12-M <masluk.v.i@gmail.com>
2026-07-07 20:31:34 +0300

    social_media_start

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,18 +4,24 @@ import styles from "../styles/Header.module.css";
 
 export default function Header () {
     return (
-        <div className={styles.Header}>
-            <div>
-                <p>Про нас</p>
-                <p>Педагоги</p>
-                <p>Стилі</p>
+        <div>
+            <div className={styles.Header}>
+                <div className={styles.row_1}>
+                    <p>Про нас</p>
+                    <p>Педагоги</p>
+                    <p>Стилі</p>
+                </div>
+                <img className={styles.row_2} src="/Logotup.png" alt="" />
+                <div className={styles.row_3}>
+                    <p>Ціни</p>
+                    <p>Розклад</p>
+                    <p>Блог</p>
+                </div>
             </div>
-            <img src="/Logotup.png" alt="" />
-            <div>
-                <p>Ціни</p>
-                <p>Розклад</p>
-                <p>Блог</p>
+            <div className={styles.Header_Phone}>
+
             </div>
         </div>
+        
     )
 }
