53897ad42053642e01462ea33b36ea850770eddc
VoVik12-M <masluk.v.i@gmail.com>
2026-07-09 20:33:53 +0300

    social_media_efect_icon_start

--- components/Social_media.tsx
+++ components/Social_media.tsx
@@ -8,20 +8,32 @@ export default function Social_media () {
     const efectu = useRef<HTMLDivElement>(null);
     const efectu2 = useRef<HTMLDivElement>(null);
 
+    const [ramkaX, setRamkaX] = useState<boolean>(false)
+    
     return (
         <div>
-            <div ref={efectu} className={styles.ramka}
-            onMouseEnter={() =>{
-                efectu.current?.classList.add(styles.efect),
+            <div ref={efectu} className={!ramkaX ? styles.ramka : styles.ramkaX}
+            onMouseMove={() =>{
+                efectu.current?.classList.add(styles.efect);
                 efectu2.current?.classList.add(styles.efecttext)
             }}
             onMouseLeave={() => {
-                efectu.current?.classList.remove(styles.efect)
+                efectu.current?.classList.remove(styles.efect);
                 efectu2.current?.classList.remove(styles.efecttext)
-            }}>
+            }}
+            onClick={() => {
+                setRamkaX(prev=>!prev)
+            }}
+            >
             </div>
-            <div className={styles.icon}>
-                <p ref={efectu2}>Social Media</p>
+            <div className={!ramkaX ? styles.icon:styles.iconX}>
+                {!ramkaX ? 
+                <p ref={efectu2}>Social Media</p> :
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                }
             </div>
         </div>
     )
